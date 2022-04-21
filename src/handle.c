3849ae5de713440e0381e17ac4b08179fe6a0054
hhvn <dev@hhvn.uk>
2022-04-21 20:34:10 +0100

    Move big struct arrays into src/data dir

--- src/handle.c
+++ src/handle.c
@@ -22,71 +22,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include "hirc.h"
-
-#define HANDLER(func) static void func(struct Server *server, struct History *msg)
-HANDLER(handle_ERROR);
-HANDLER(handle_PING);
-HANDLER(handle_PONG);
-HANDLER(handle_JOIN);
-HANDLER(handle_PART);
-HANDLER(handle_KICK);
-HANDLER(handle_QUIT);
-HANDLER(handle_NICK);
-HANDLER(handle_MODE);
-HANDLER(handle_TOPIC);
-HANDLER(handle_PRIVMSG);
-HANDLER(handle_INVITE);
-HANDLER(handle_RPL_WELCOME);
-HANDLER(handle_RPL_ISUPPORT);
-HANDLER(handle_RPL_CHANNELMODEIS);
-HANDLER(handle_RPL_NOTOPIC);
-HANDLER(handle_RPL_TOPIC);
-HANDLER(handle_RPL_TOPICWHOTIME);
-HANDLER(handle_RPL_INVITING);
-HANDLER(handle_RPL_NAMREPLY);
-HANDLER(handle_RPL_ENDOFNAMES);
-HANDLER(handle_RPL_MOTD);
-HANDLER(handle_RPL_ENDOFMOTD);
-HANDLER(handle_ERR_NOSUCHNICK);
-HANDLER(handle_ERR_NICKNAMEINUSE);
-HANDLER(handle_RPL_AWAY);
-
-struct Ignore *ignores = NULL;
-struct Handler handlers[] = {
-	{ "ERROR",	handle_ERROR			},
-	{ "PING", 	handle_PING			},
-	{ "PONG",	handle_PONG			},
-	{ "JOIN",	handle_JOIN			},
-	{ "PART",	handle_PART			},
-	{ "KICK",	handle_KICK			},
-	{ "QUIT",	handle_QUIT			},
-	{ "NICK",	handle_NICK			},
-	{ "MODE",	handle_MODE			},
-	{ "TOPIC",	handle_TOPIC			},
-	{ "PRIVMSG",	handle_PRIVMSG  		},
-	{ "NOTICE",	handle_PRIVMSG	  		},
-	{ "INVITE",	handle_INVITE			},
-	{ "001",	handle_RPL_WELCOME		},
-	{ "005",	handle_RPL_ISUPPORT		},
-	{ "301",	handle_RPL_AWAY			},
-	{ "324",	handle_RPL_CHANNELMODEIS	},
-	{ "331",	handle_RPL_NOTOPIC		},
-	{ "329",	NULL				}, /* ignore this:
-							    *  - it's nonstandard
-							    *  - hirc has no use for it currently
-							    *  - it's annoyingly sent after MODE */
-	{ "332",	handle_RPL_TOPIC		},
-	{ "333",	handle_RPL_TOPICWHOTIME		},
-	{ "341",	handle_RPL_INVITING		},
-	{ "353",	handle_RPL_NAMREPLY		},
-	{ "366",	handle_RPL_ENDOFNAMES		},
-	{ "372",	handle_RPL_MOTD			},
-	{ "375",	handle_RPL_MOTD			}, /* RPL_MOTDSTART, but handle it the same way as RPL_MOTD */
-	{ "376",	handle_RPL_ENDOFMOTD		},
-	{ "401",	handle_ERR_NOSUCHNICK		},
-	{ "433",	handle_ERR_NICKNAMEINUSE	},
-	{ NULL,		NULL 				},
-};
+#include "data/handlers.h"
 
 HANDLER(
 handle_PING) {
